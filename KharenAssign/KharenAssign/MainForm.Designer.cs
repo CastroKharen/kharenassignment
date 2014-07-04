f73da4bf0437a0ed033dad4fae98643f28236ae9
castrokharen <nerahkcastro123@gmail.com>
2014-07-04 17:15:45 +0800

    assignment

---
+++ KharenAssign/KharenAssign/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace KharenAssign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(78, 54);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(108, 55);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "1";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 160);
+            this.Controls.Add(this.btnMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+    }
+}
+
---
+++ KharenAssign/KharenAssign/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KharenAssign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Form2 frm2 = new Form2();
+                frm2.ShowDialog();
+
+                int k = Convert.ToInt16(frm2.txt.Text) + Convert.ToInt16(btnMain.Text);
+                btnMain.Text = k.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }            
+        }
+    }
+}
